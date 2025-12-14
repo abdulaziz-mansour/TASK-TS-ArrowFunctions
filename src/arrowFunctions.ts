1e468f15c4d81f72a561965232c8de1111dc2807
abdulaziz-mansour <abdulaziz.mansour.work@gmail.com>
2025-12-14 14:41:10 +0300

    solve the task

--- src/arrowFunctions.ts
+++ src/arrowFunctions.ts
@@ -4,13 +4,24 @@
  * The arrow function must have the **same type declaration** as the original function
  */
 
+import { az } from "@faker-js/faker/.";
+
 // Comment out this function before starting your solution
-function greet(name: string): string {
-  return `Hello ${name}`;
+function greet(Aziz: string): string {
+  return `Hello ${Aziz}`;
 }
 
 // example:
 greet("Aziz"); // => "Hello Aziz"
+
+
+
+
+
+
+
+
+
 
 /**
  * sum(a, b):
@@ -24,12 +35,28 @@ greet("Aziz"); // => "Hello Aziz"
  */
 
 // Comment out this function before starting your solution
-function sum(a: number, b: number): number {
-  return Infinity;
-}
 
+// function sum(a: number, b: number): number {
+// }
+const sum = (a: number, b: number): number => {
+  return a + b;
+}
+  // return Infinity;
 // example:
+
+//console.log(sum(5,7));
 sum(5, 7); // => 12
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * square(n):
@@ -41,12 +68,26 @@ sum(5, 7); // => 12
  */
 
 // Comment out this function before starting your solution
-function square(n: number): number {
-  return Infinity;
+
+
+// function square(n: number): number {
+//   return Infinity;
+// }
+const square = (n: number): number => {
+  return n * n;
 }
 
+// console.log(square(4));
+// console.log(square(10));
 // example:
 square(4); // => 16
 square(10); // => 100
+
+
+
+
+
+
+
 
 export { greet, sum, square };
